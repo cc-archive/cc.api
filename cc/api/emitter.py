bd5f995ea005c4e8af8265ba495b749a72adc0e6
John Doig <jed@jedpad.(none)>
2010-02-25 23:19:42 -0800

    XMLEmitter class that converts dictionaries to an ElementTree

--- cc/api/emitter.py
+++ cc/api/emitter.py
@@ -18,23 +18,45 @@
 ## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ## DEALINGS IN THE SOFTWARE.
 
-import lxml.etree
+import lxml.etree as ET
 import web
 
 def processor(handler):
     results = handler()
     if web.input().get('format', False):
-        # use the emitter
-        return results
-    else:
-        # use xml emitter
-        return XMLEmitter().format(results)
+        # determine if there an available emitter for the requested format
+        # only concerned with XML for now
+        pass
+    
+    return XMLEmitter().format(results)
     
 class XMLEmitter:
-    
+
+    def build_tree(self, parent, children):
+        for child in children:
+            if type(children[child]) == list:
+                for obj in children[child]:
+                    if type(obj) == list:
+                        ele = ET.Element(child)
+                        self.build_tree(ele, obj)
+                    elif type(obj == dict):
+                        ele = ET.Element(child, obj.get('attributes',None))
+                        ele.text = obj.get('text',None)
+                    parent.append(ele)
+            else:
+                ele = ET.Element(child)
+                if type(children[child]) == dict:
+                    ele.attrib = children[child].get('attributes', None)
+                    ele.text = children[child].get('text', None)
+                parent.append(child)        
+        return parent
+
     def dict_to_etree(self, d):
-        return d
+        top = list(d)[0]
+        root = ET.Element(top, d[top].get('attributes', None))
+        root.text = d[top].get('text', None)
+        return self.build_tree(root, d[top])
     
     def format(self, results_dict):
-        return self.dict_to_etree(results_dict)
+        return ET.tostring(self.dict_to_etree(results_dict))
         
