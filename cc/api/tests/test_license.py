ef05168814c2cb1e18abe4e4bd6713c3b494184a
John Doig <jed@jedpad.(none)>
2010-03-12 18:14:32 -0800

    Adding test cases for work-info logic

--- cc/api/tests/test_license.py
+++ cc/api/tests/test_license.py
@@ -1,5 +1,7 @@
 
 import os
+from urllib import urlencode
+import itertools
 
 from cc.api.tests.test_common import *
 
@@ -82,11 +84,16 @@ class TestLicenseIssue(TestApi):
         """Issue with no answers or empty answers should return an error."""
         res = self.app.post('/license/blarf/issue')
         assert relax_validate(RELAX_ERROR, res.body)
+        res = self.app.post('/license/standard/issue')
+        assert relax_validate(RELAX_ERROR, res.body)
 
     def test_invalid_answers(self):
         """Invalid answer string returns error."""
         res = self.app.post('/license/standard/issue',
                                 params={'answers':'<foo/>'})
+        assert relax_validate(RELAX_ERROR, res.body)
+        res = self.app.post('/license/standard/issue',
+                                params={'answers':'<answers><locale>en</loca'})
         assert relax_validate(RELAX_ERROR, res.body)
 
     def _issue(self, lclass):
@@ -137,14 +144,110 @@ class TestLicenseIssue(TestApi):
                                 params={'answers':answers})
         assert relax_validate(RELAX_ERROR, res.body)
 
+class TestLicenseWorkInfo(TestApi):
+
+    _fields = {
+        'title': 'Testing Title',
+        'attribution_url': 'http://example.com/work-url',
+        'source-url': 'http://example.com/source-url',
+        'type': 'Text',
+        'more_permissions_url': 'http://example.com/more-permissions-url',
+        }        
+    _cc0_fields = {
+        'title': 'Testing Title',
+        'actor_href': 'http://example.com',
+        'name': 'Testing',
+        'work_jurisdiction': 'US',
+        }
+        
+    _answers = '<answers><locale>en</locale><license-standard>' + \
+              '<commercial>y</commercial>' + \
+              '<derivatives>y</derivatives>' + \
+              '<jurisdiction></jurisdiction>' + \
+              '</license-standard>%s</answers>'
+    
+    _cc0_answers = '<answers><license-zero />%s</answers>'
+    
+    def _work_info_string(self, fields):
+        root = "<work-info>"
+        for k,v in fields.items():
+            root += "<%(key)s>%(value)s</%(key)s>" % {'key':k,'value':v}
+        root += "</work-info>"
+        return root
+
+    def _permute_workinfo(self, fields, values=None):
+        if values == None:
+            values = self._fields
+        work_infos = []
+        for i in range(1, len(fields) + 1):
+            for c in itertools.combinations(fields, i):
+                workfields = dict([ (f, values[f]) for f in c])
+                work_infos.append(workfields)
+        return work_infos
+
+    def test_license_workinfo(self):
+        """ get & issue should return identical (and valid) responses based on the
+        data provided in work-info. """
+
+        work_infos = self._permute_workinfo(self._fields.keys())
+        
+        for workinfo in work_infos:
+            wi = self._work_info_string(
+                dict([ (f, self._fields[f]) for f in workinfo ]))
+            i = self.app.post('/license/standard/issue',
+                              params={'answers':self._answers % wi})
+            g = self.app.get('/license/standard/get?' + urlencode(workinfo))
+            
+            for f in workinfo:
+                # assert field is present in issue response
+                assert self._fields[f] in i.body, '%s=%s not in issue results.' % \
+                       (f, self._fields[f])
+                # assert field is present in get response
+                assert self._fields[f] in g.body, '%s=%s not in get results.' % \
+                       (f, self._fields[f])
+                # issue and get should be identical
+                assert i.body == g.body
+                
+        work_infos = self._permute_workinfo(self._cc0_fields.keys(), self._cc0_fields)
+        for workinfo in work_infos:
+            wi = self._work_info_string(
+                dict([ (f, self._cc0_fields[f]) for f in workinfo ]))
+            i = self.app.post('/license/zero/issue',
+                              params={'answers':self._cc0_answers % wi})
+            g = self.app.get('/license/zero/get?' + urlencode(workinfo))
+            for f in workinfo:
+                # assert field is present in issue response
+                assert self._cc0_fields[f] in i.body, '%s=%s not in issue results.'% \
+                       (f, self._cc0_fields[f])
+                # assert field is present in get response
+                assert self._cc0_fields[f] in g.body, '%s=%s not in get results.' % \
+                       (f, self._cc0_fields[f])
+                # issue and get should be identical
+                assert i.body == g.body
+
+    def test_invalid_work_jurisdiction(self):
+        """ cc.license will KeyError when an invalid work_jurisidction is passed to
+        the CC0 formatter. """
+        res = self.app.get('/license/zero/get?work_jurisdiction=PUKE')
+        assert relax_validate(RELAX_ERROR, res.body)
+        res = self.app.post('/license/zero/issue',
+                            params={'answers': self._cc0_answers % (
+                                    '<work-info><work_jurisdiction>' + \
+                                    'PUKE</work_jurisdiction></work-info>')})
+        assert relax_validate(RELAX_ERROR, res.body)
 
 class TestLicenseGet(TestApi):
-
+    
     def test_invalid_class(self):
         """/get should return an error with an invalid class."""
         res = self.app.get('/license/%s/get' % hash(self))
         assert relax_validate(RELAX_ERROR, res.body)
 
+    def test_invalid_answers(self):
+        """/get should return an error for invalid answers."""
+        res = self.app.get('/license/standard/get?commercial=z')
+        assert relax_validate(RELAX_ERROR, res.body)
+    
     def test_ignore_extra_args(self):
         """Test /get ignores extra nonsense arguments."""
         for lclass in self.data.license_classes():
